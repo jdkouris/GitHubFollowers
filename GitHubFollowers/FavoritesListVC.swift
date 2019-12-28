e2497b0f18f27f50b582093655995da2469e3067
John Kouris <jdkouris@gmail.com>
2019-12-28 15:32:48 -0800

    Set up the search and favorites VCs in the SceneDelegate

---
+++ GitHubFollowers/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by John Kouris on 12/28/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+}
