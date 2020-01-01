e4a2380b7c84535103a5bf9b1efb823e851d9966
John Kouris <jdkouris@gmail.com>
2019-12-31 16:58:21 -0800

    Added the user and follower models

---
+++ GitHubFollowers/Models/User.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  GitHubFollowers
+//
+//  Created by John Kouris on 12/31/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
