414d9628da10ce2f93d6cfc98482e56c7ea6c849
John Kouris <jdkouris@gmail.com>
2020-01-23 14:45:18 -0800

    Added the user info header view

---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by John Kouris on 1/23/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
