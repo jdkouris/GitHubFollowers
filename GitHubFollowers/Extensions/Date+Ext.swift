4fd2821d6d307efab2dc31dbd6a171d26b4aa515
John Kouris <jdkouris@gmail.com>
2020-01-26 08:08:44 -0800

    Added the date formatter to format the createdAt date and display it in the dateLabel in the UserInfoVC

---
+++ GitHubFollowers/Extensions/Date+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  Date+Ext.swift
+//  GitHubFollowers
+//
+//  Created by John Kouris on 1/26/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    
+    func convertToMonthYearFormat() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
