04798495de18b99e4bc157af692a8f552d6b1fab
John Kouris <jdkouris@gmail.com>
2020-01-26 07:43:23 -0800

    Added the GFRepoItemVC Populating the UserInfoVC itemViews with the GFFollowerItemVC and the GFRepoItemVC

---
+++ GitHubFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,24 @@
+//
+//  GFFollowerItemVC.swift
+//  GitHubFollowers
+//
+//  Created by John Kouris on 1/26/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+    
+}
---
+++ GitHubFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFRepoItemVC.swift
@@ -0,0 +1,24 @@
+//
+//  GFRepoItemVC.swift
+//  GitHubFollowers
+//
+//  Created by John Kouris on 1/26/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+    
+}
