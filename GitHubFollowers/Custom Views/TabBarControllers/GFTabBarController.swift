54ac2a9e9be6b9f749f5a53eaa4bd7efbb366509
John Kouris <jdkouris@gmail.com>
2020-02-20 09:52:27 -0800

    Optimized the tab bar, screen sizes, and some other changes

---
+++ GitHubFollowers/Custom Views/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  GFTabBarController.swift
+//  GitHubFollowers
+//
+//  Created by John Kouris on 2/20/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers = [createSearchNC(), createFavoritesNC()]
+    }
+    
+    func createSearchNC() -> UINavigationController {
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesNC() -> UINavigationController {
+        let favoritesListVC = FavoritesListVC()
+        favoritesListVC.title = "Favorites"
+        favoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesListVC)
+    }
+
+}
