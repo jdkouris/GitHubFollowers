9556d71e1a71b7be381aa9187d2795d70a637cfe
John Kouris <jdkouris@gmail.com>
2021-02-03 10:48:16 -0800

    Added UserInfoVC content inside a scrollView

--- GitHubFollowers/Extensions/UIView+Ext.swift
+++ GitHubFollowers/Extensions/UIView+Ext.swift
@@ -16,4 +16,14 @@ extension UIView {
         }
     }
     
+    func pinToEdges(of superview: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superview.topAnchor),
+            leadingAnchor.constraint(equalTo: superview.leadingAnchor),
+            trailingAnchor.constraint(equalTo: superview.trailingAnchor),
+            bottomAnchor.constraint(equalTo: superview.bottomAnchor)
+        ])
+    }
+    
 }
